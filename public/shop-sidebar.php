42b4603172a91636d97ea448cd707b6f8d25407f
Alik Davtyan <alikwotdav@gmail.com>
2026-03-12 15:36:18 +0200

    Rebuild category sidebar hiding for WPBakery archives

--- public/shop-sidebar.php
+++ public/shop-sidebar.php
@@ -3,11 +3,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action( 'template_redirect', 'palaplast_maybe_hide_shop_sidebar', 20 );
-add_filter( 'body_class', 'palaplast_add_no_shop_sidebar_body_class' );
-add_action( 'wp_enqueue_scripts', 'palaplast_enqueue_shop_sidebar_fallback_styles' );
+add_filter( 'body_class', 'palaplast_add_product_cat_filter_state_body_class' );
+add_action( 'wp_enqueue_scripts', 'palaplast_enqueue_product_cat_filter_state_assets' );
 
-function palaplast_should_hide_shop_sidebar() {
+function palaplast_should_hide_product_cat_filters() {
 	static $should_hide = null;
 
 	if ( null !== $should_hide ) {
@@ -21,11 +20,11 @@ function palaplast_should_hide_shop_sidebar() {
 	}
 
 	$current_term = get_queried_object();
-	if ( ! $current_term instanceof WP_Term || 'product_cat' !== $current_term->taxonomy ) {
+	if ( ! ( $current_term instanceof WP_Term ) || 'product_cat' !== $current_term->taxonomy ) {
 		return $should_hide;
 	}
 
-	$child_term_ids = get_terms(
+	$child_terms = get_terms(
 		array(
 			'taxonomy'   => 'product_cat',
 			'parent'     => (int) $current_term->term_id,
@@ -35,32 +34,25 @@ function palaplast_should_hide_shop_sidebar() {
 		)
 	);
 
-	if ( is_wp_error( $child_term_ids ) || empty( $child_term_ids ) ) {
+	if ( is_wp_error( $child_terms ) || empty( $child_terms ) ) {
 		return $should_hide;
 	}
 
 	$should_hide = true;
+
 	return $should_hide;
 }
 
-function palaplast_maybe_hide_shop_sidebar() {
-	if ( ! palaplast_should_hide_shop_sidebar() ) {
-		return;
-	}
-
-	remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
-}
-
-function palaplast_add_no_shop_sidebar_body_class( $classes ) {
-	if ( palaplast_should_hide_shop_sidebar() ) {
-		$classes[] = 'myplugin-no-shop-sidebar';
+function palaplast_add_product_cat_filter_state_body_class( $classes ) {
+	if ( palaplast_should_hide_product_cat_filters() ) {
+		$classes[] = 'palaplast-hide-product-cat-filters';
 	}
 
 	return $classes;
 }
 
-function palaplast_enqueue_shop_sidebar_fallback_styles() {
-	if ( ! palaplast_should_hide_shop_sidebar() ) {
+function palaplast_enqueue_product_cat_filter_state_assets() {
+	if ( ! palaplast_should_hide_product_cat_filters() ) {
 		return;
 	}
 
@@ -69,5 +61,13 @@ function palaplast_enqueue_shop_sidebar_fallback_styles() {
 		PALAPLAST_PLUGIN_URL . 'public/css/shop-sidebar.css',
 		array(),
 		PALAPLAST_VERSION
+	);
+
+	wp_enqueue_script(
+		'palaplast-shop-sidebar',
+		PALAPLAST_PLUGIN_URL . 'public/js/shop-sidebar.js',
+		array(),
+		PALAPLAST_VERSION,
+		true
 	);
 }
