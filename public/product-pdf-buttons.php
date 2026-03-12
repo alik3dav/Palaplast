7f29a0b682c8c1cfe839fee96ca47558899090e0
Alik Davtyan <alikwotdav@gmail.com>
2026-03-12 13:36:39 +0200

    Add shortcode and builder support for product PDF buttons

--- public/product-pdf-buttons.php
+++ public/product-pdf-buttons.php
@@ -3,25 +3,85 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+
+function &palaplast_get_rendered_pdf_buttons() {
+	static $rendered = array(
+		'technical_sheet' => array(),
+		'pricelist'       => array(),
+	);
+
+	return $rendered;
+}
+
+function palaplast_pdf_button_already_rendered( $type, $product_id ) {
+	$rendered = &palaplast_get_rendered_pdf_buttons();
+
+	$product_id = (int) $product_id;
+	if ( ! $product_id || empty( $rendered[ $type ] ) ) {
+		return false;
+	}
+
+	return ! empty( $rendered[ $type ][ $product_id ] );
+}
+
+function palaplast_mark_pdf_button_rendered( $type, $product_id ) {
+	$rendered = &palaplast_get_rendered_pdf_buttons();
+
+	$product_id = (int) $product_id;
+	if ( ! $product_id || ! isset( $rendered[ $type ] ) ) {
+		return;
+	}
+
+	$rendered[ $type ][ $product_id ] = true;
+}
+
 function palaplast_render_technical_sheet_button() {
 	if ( ! function_exists( 'is_product' ) || ! is_product() ) {
 		return;
 	}
 
-	global $product;
+	palaplast_render_technical_sheet_button_for_product();
+}
+
+function palaplast_render_technical_sheet_button_for_product( $product_id = 0, $return_html = false ) {
+	$product = palaplast_get_product_for_pdf_output( $product_id );
+
 	if ( ! $product instanceof WC_Product ) {
-		return;
+		return $return_html ? '' : null;
 	}
 
-	$sheet = palaplast_get_product_technical_sheet( $product->get_id() );
-	if ( empty( $sheet['file_url'] ) ) {
-		return;
+	$product_id = $product->get_id();
+
+	if ( function_exists( 'palaplast_get_current_language_product_id' ) ) {
+		$product_id = palaplast_get_current_language_product_id( $product_id );
 	}
+
+	if ( ! $return_html && palaplast_pdf_button_already_rendered( 'technical_sheet', $product_id ) ) {
+		return null;
+	}
+
+	$sheet = palaplast_get_product_technical_sheet( $product_id );
+	if ( empty( $sheet['file_url'] ) ) {
+		return $return_html ? '' : null;
+	}
+
+	ob_start();
 	?>
 	<p class="palaplast-technical-sheet">
 		<a class="button palaplast-technical-sheet-button" href="<?php echo esc_url( $sheet['file_url'] ); ?>" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Download Technical Sheet', 'palaplast' ); ?></a>
 	</p>
 	<?php
+
+	$output = ob_get_clean();
+
+	if ( $return_html ) {
+		return $output;
+	}
+
+	palaplast_mark_pdf_button_rendered( 'technical_sheet', $product_id );
+	echo $output;
+
+	return null;
 }
 
 function palaplast_render_pricelist_button() {
@@ -29,18 +89,95 @@ function palaplast_render_pricelist_button() {
 		return;
 	}
 
-	global $product;
+	palaplast_render_pricelist_button_for_product();
+}
+
+function palaplast_render_pricelist_button_for_product( $product_id = 0, $return_html = false ) {
+	$product = palaplast_get_product_for_pdf_output( $product_id );
+
 	if ( ! $product instanceof WC_Product ) {
-		return;
+		return $return_html ? '' : null;
 	}
 
-	$pricelist = palaplast_get_product_pricelist( $product->get_id() );
-	if ( empty( $pricelist['file_url'] ) ) {
-		return;
+	$product_id = $product->get_id();
+
+	if ( function_exists( 'palaplast_get_current_language_product_id' ) ) {
+		$product_id = palaplast_get_current_language_product_id( $product_id );
 	}
+
+	if ( ! $return_html && palaplast_pdf_button_already_rendered( 'pricelist', $product_id ) ) {
+		return null;
+	}
+
+	$pricelist = palaplast_get_product_pricelist( $product_id );
+	if ( empty( $pricelist['file_url'] ) ) {
+		return $return_html ? '' : null;
+	}
+
+	ob_start();
 	?>
 	<p class="palaplast-pricelist">
 		<a class="button palaplast-pricelist-button" href="<?php echo esc_url( $pricelist['file_url'] ); ?>" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Download Pricelist', 'palaplast' ); ?></a>
 	</p>
 	<?php
+
+	$output = ob_get_clean();
+
+	if ( $return_html ) {
+		return $output;
+	}
+
+	palaplast_mark_pdf_button_rendered( 'pricelist', $product_id );
+	echo $output;
+
+	return null;
+}
+
+function palaplast_technical_sheet_button_shortcode( $atts ) {
+	$atts = shortcode_atts(
+		array(
+			'product_id' => 0,
+		),
+		$atts,
+		'palaplast_technical_sheet'
+	);
+
+	return palaplast_render_technical_sheet_button_for_product( (int) $atts['product_id'], true );
+}
+
+function palaplast_pricelist_button_shortcode( $atts ) {
+	$atts = shortcode_atts(
+		array(
+			'product_id' => 0,
+		),
+		$atts,
+		'palaplast_pricelist_pdf'
+	);
+
+	return palaplast_render_pricelist_button_for_product( (int) $atts['product_id'], true );
+}
+
+function palaplast_get_product_for_pdf_output( $product_id = 0 ) {
+	$product_id = (int) $product_id;
+
+	if ( $product_id ) {
+		$product = wc_get_product( $product_id );
+		if ( $product instanceof WC_Product ) {
+			return $product;
+		}
+	}
+
+	global $product;
+	if ( $product instanceof WC_Product ) {
+		return $product;
+	}
+
+	$current_post_id = get_the_ID();
+	if ( ! $current_post_id ) {
+		return null;
+	}
+
+	$product = wc_get_product( $current_post_id );
+
+	return $product instanceof WC_Product ? $product : null;
 }
